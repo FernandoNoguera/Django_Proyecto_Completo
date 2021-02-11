f7317539781b60dbbeff003a1385c8e671e448de
FernandoNoguera <fnoguerav25@gmail.com>
2021-02-11 13:20:52 -0300

    se agregan los servicios de la bd al view para que se visualicen en el template

--- ProyectoWebApp/views.py
+++ ProyectoWebApp/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render, HttpResponse
 
+from servicios.models import Servicio
+
 def home(request):
     return render(request, 'ProyectoWebApp/home.html')
 
 def servicios(request):
-    return render(request, 'ProyectoWebApp/servicios.html')
+    servicios = Servicio.objects.all()#se le indica q taiga los servivios de la base de datos
+    print(servicios)
+    context = {'servicios':servicios}
+
+    return render(request, 'ProyectoWebApp/servicios.html', context)
 
 
 def tienda(request):
