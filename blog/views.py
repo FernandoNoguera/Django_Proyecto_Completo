bc2482e6800981ebd10614039f5c247a4bf73325
FernandoNoguera <fnoguerav25@gmail.com>
2021-02-12 10:15:03 -0300

    se añaden funcionalidades a las categorias

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 
-from blog.models import Post
+from blog.models import Post, Categoria
 
 def blog(request):
     posteos = Post.objects.all()
@@ -8,4 +8,11 @@ def blog(request):
 
     return render(request, 'blog/blog.html', context)
 
+
+def categoria(request, categoria_id):
+    categoria = Categoria.objects.get(id=categoria_id)
+    posteos = Post.objects.filter(categorias=categoria)
+    context = {'posteos':categoria, 'posteos':posteos}
+
+    return render(request, 'blog/categoria.html', context)
 
